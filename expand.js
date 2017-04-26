250459303921d834f4cfa2927db185f9ab9ca33b
Whitaker <whitaker.brand@gmail.com>
2017-04-26 05:46:12 +0000

    added bootstrap to entire site

--- expand.js
+++ expand.js
@@ -1,26 +1,31 @@
 /*
 Brian Hart
-4/14/17
+4/25/17
 CSE 154 AC
 TA: Yen Lee
-Creative Assignment 3 
+Creative Assignment 4 
 This file contains a script to expand the description of the projects on the projects page.
 */
 
-"use strict";
-
 (function() {
+    "use strict";
     window.onload =function() {
+        
         var open = false;
-        document.getElementById("circleshelftitle").onclick = function() {
-            if (!open){
+
+        document.getElementById("CC-button").onclick = expand;
+        
+        ('.collapse').collapse()
+        function expand() {
+            if (!open) {
                 document.getElementById("description").style.height = "370px";
                 open = true;
             } else if (open) {
                 document.getElementById("description").style.height = "0px";
                 open = false;
-            }
-        };    
+            }    
+        }
     };
+});
     
 })();
